8dd39acf4a8f3c2e75049339a1b140f8b6ecee78
wanderer-fly <wand.liulang@gmail.com>
2025-04-24 00:34:21 +0800

    1

--- routes/user.js
+++ routes/user.js
@@ -53,7 +53,7 @@ router.post('/login', async (req, res) => {
 
 // 注册
 router.post('/register', async (req, res) => {
-    const isAllowed = false; // 设置是否允许注册
+    const isAllowed = true; // 设置是否允许注册
 
     if (!isAllowed) {
         return res.status(400).json({ message: '当前不可注册' });
